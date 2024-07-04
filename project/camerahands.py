1851b984f8eab3fd5feccc74381149adbd11f3d3
Giviko28 <chelidze.givia@gmail.com>
2024-07-04 19:05:22 +0200

    added threading so it doesnt freeze after pressing space

--- project/camerahands.py
+++ project/camerahands.py
@@ -7,6 +7,7 @@ from keras.models import load_model
 import numpy as np
 import time
 import pandas as pd
+import threading
 
 filepath = os.path.dirname(__file__)
 model = load_model(f'{filepath}/smnist.h5')
@@ -23,6 +24,72 @@ h, w, c = frame.shape
 img_counter = 0
 analysisframe = ''
 letterpred = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y']
+
+def analyze_frame(frame):
+    showframe = frame
+    cv2.imshow("Frame", showframe)
+    framergbanalysis = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    resultanalysis = hands.process(framergbanalysis)
+    hand_landmarksanalysis = resultanalysis.multi_hand_landmarks
+    if hand_landmarksanalysis:
+        for handLMsanalysis in hand_landmarksanalysis:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lmanalysis in handLMsanalysis.landmark:
+                x, y = int(lmanalysis.x * w), int(lmanalysis.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            y_min -= 20
+            y_max += 20
+            x_min -= 20
+            x_max += 20 
+
+    analysisframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    analysisframe = analysisframe[y_min:y_max, x_min:x_max]
+    analysisframe = cv2.resize(analysisframe,(28,28))
+    nlist = []
+    rows, cols = analysisframe.shape
+    for i in range(rows):
+        for j in range(cols):
+            k = analysisframe[i,j]
+            nlist.append(k)
+    
+    datan = pd.DataFrame(nlist).T
+    colname = []
+    for val in range(784):
+        colname.append(val)
+    datan.columns = colname
+
+    pixeldata = datan.values
+    pixeldata = pixeldata / 255
+    pixeldata = pixeldata.reshape(-1, 28, 28, 1)
+    prediction = model.predict(pixeldata)
+    predarray = np.array(prediction[0])
+    letter_prediction_dict = {letterpred[i]: predarray[i] for i in range(len(letterpred))}
+    predarrayordered = sorted(predarray, reverse=True)
+    high1 = predarrayordered[0]
+    high2 = predarrayordered[1]
+    high3 = predarrayordered[2]
+    for key, value in letter_prediction_dict.items():
+        if value == high1:
+            print("Predicted Character 1: ", key)
+            print('Confidence 1: ', 100*value)
+        elif value == high2:
+            print("Predicted Character 2: ", key)
+            print('Confidence 2: ', 100*value)
+        elif value == high3:
+            print("Predicted Character 3: ", key)
+            print('Confidence 3: ', 100*value)
+    time.sleep(5)
+
 while True:
     _, frame = cap.read()
 
@@ -34,71 +101,9 @@ while True:
     elif k%256 == 32:
         # SPACE pressed
         analysisframe = frame
-        showframe = analysisframe
-        cv2.imshow("Frame", showframe)
-        framergbanalysis = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2RGB)
-        resultanalysis = hands.process(framergbanalysis)
-        hand_landmarksanalysis = resultanalysis.multi_hand_landmarks
-        if hand_landmarksanalysis:
-            for handLMsanalysis in hand_landmarksanalysis:
-                x_max = 0
-                y_max = 0
-                x_min = w
-                y_min = h
-                for lmanalysis in handLMsanalysis.landmark:
-                    x, y = int(lmanalysis.x * w), int(lmanalysis.y * h)
-                    if x > x_max:
-                        x_max = x
-                    if x < x_min:
-                        x_min = x
-                    if y > y_max:
-                        y_max = y
-                    if y < y_min:
-                        y_min = y
-                y_min -= 20
-                y_max += 20
-                x_min -= 20
-                x_max += 20 
-
-        analysisframe = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2GRAY)
-        analysisframe = analysisframe[y_min:y_max, x_min:x_max]
-        # lol = cv2.resize(analysisframe, (128,128))
-        # cv2.imshow("tst", analysisframe)
-        analysisframe = cv2.resize(analysisframe,(28,28))
-        nlist = []
-        rows,cols = analysisframe.shape
-        for i in range(rows):
-            for j in range(cols):
-                k = analysisframe[i,j]
-                nlist.append(k)
-        
-        datan = pd.DataFrame(nlist).T
-        colname = []
-        for val in range(784):
-            colname.append(val)
-        datan.columns = colname
-
-        pixeldata = datan.values
-        pixeldata = pixeldata / 255
-        pixeldata = pixeldata.reshape(-1,28,28,1)
-        prediction = model.predict(pixeldata)
-        predarray = np.array(prediction[0])
-        letter_prediction_dict = {letterpred[i]: predarray[i] for i in range(len(letterpred))}
-        predarrayordered = sorted(predarray, reverse=True)
-        high1 = predarrayordered[0]
-        high2 = predarrayordered[1]
-        high3 = predarrayordered[2]
-        for key,value in letter_prediction_dict.items():
-            if value==high1:
-                print("Predicted Character 1: ", key)
-                print('Confidence 1: ', 100*value)
-            elif value==high2:
-                print("Predicted Character 2: ", key)
-                print('Confidence 2: ', 100*value)
-            elif value==high3:
-                print("Predicted Character 3: ", key)
-                print('Confidence 3: ', 100*value)
-        time.sleep(5)
+        # Start a new thread to analyze the frame
+        analysis_thread = threading.Thread(target=analyze_frame, args=(analysisframe,))
+        analysis_thread.start()
 
     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     result = hands.process(framergb)
