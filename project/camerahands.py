28c7405e559fbe7a9c020bd04642b5d489377ade
Giviko28 <chelidze.givia@gmail.com>
2024-07-04 18:57:50 +0200

    dookie

--- project/camerahands.py
+++ project/camerahands.py
@@ -8,7 +8,8 @@ import numpy as np
 import time
 import pandas as pd
 
-model = load_model('smnist.h5')
+filepath = os.path.dirname(__file__)
+model = load_model(f'{filepath}/smnist.h5')
 
 mphands = mp.solutions.hands
 hands = mphands.Hands()
